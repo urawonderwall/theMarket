1ed4fdd9082a06811a405b4c1f17ef10379cf666
Olga Isela Ceballos Miranda <olga.ceballos@ibm.com>
2021-01-06 05:14:34 -0600

    My market structure.

---
+++ src/main/java/com/the/market/TheMarketApplication.java
@@ -0,0 +1,13 @@
+package com.the.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the/market/TheMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
