2208b65480251e4ceba1bf55fea4e633e4ec3c6d
Olga Isela Ceballos Miranda <olga.ceballos@ibm.com>
2021-01-06 08:05:30 -0600

    My first endpoint on Springboot.

---
+++ src/main/java/com/the/market/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.the.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Never stop learning!!";
+    }
+}
