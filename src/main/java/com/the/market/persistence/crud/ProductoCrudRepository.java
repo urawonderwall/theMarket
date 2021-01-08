11ad8224442fdbe178e461520d8ec1669018f033
Olga Isela Ceballos Miranda <olga.ceballos@ibm.com>
2021-01-08 06:50:22 -0600

    Definition of the methods for getting the data from database in the repository class and Interface.

--- src/main/java/com/the/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/the/market/persistence/crud/ProductoCrudRepository.java
@@ -4,8 +4,10 @@ import com.the.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
 
-    List<Producto> findByIdCategoria(int idCategoria);
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
