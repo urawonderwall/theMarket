a55ffc7355fda419e5e40e8bf69ed5792f9c9e13
Olga Isela Ceballos Miranda <olga.ceballos@ibm.com>
2021-02-10 01:04:33 -0600

    ProductoRepository is now extending from ProductRepository, I've adapted each of the methods in PR.

--- src/main/java/com/the/market/domain/repository/ProductRepository.java
+++ src/main/java/com/the/market/domain/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface ProductRepository {
     List<Product> getAll();
-    Optional<List<Product>> getByCategory(int CategoryId);
+    Optional<List<Product>> getByCategory(int categoryId);
     Optional<List<Product>> getScarseProducts(int quantity);
     Optional<Product> getProduct(int productId);
     Product save(Product product);
