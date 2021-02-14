bc60e38d3e7e7c7ff7b31f43107a76c48b8e14fb
Olga Isela Ceballos Miranda <olga.ceballos@ibm.com>
2021-02-13 18:48:47 -0600

    Exposing the endpoints.

--- src/main/java/com/the/market/web/controller/ProductController.java
+++ src/main/java/com/the/market/web/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.the.market.web.controller;
 import com.the.market.domain.Product;
 import com.the.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/all")
     public List<Product> getAll(){
         return productService.getAll();
     }
